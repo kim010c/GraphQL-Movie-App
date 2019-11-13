7e210432160ff82f0ae4bc81aef90655e11c2bd4
Park Jongho <kim010c@gmail.com>
2019-11-13 12:24:21 +0900

    챕터 #6
    Details Route and css

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Apollo boost는 GraphQL 클라이언트를 가지기 위해 모든걸 대신 셋
 Apollo boost가 GraphQL을 위한 클라이언트
 
 yarn add react-helmet //문서 헤드에 대한 모든변경사항 관라
+yarn add styled-components //스타일 컴포넌트
+yarn add styled-reset
--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import client from "./apolloClient";
 import Home from "./Home";
 import Detail from "./Detail";
+
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <React.Fragment>
+          <main>
             <Route exact={true} path={"/"} component={Home} />
             <Route path={"/details/:movieId"} component={Detail} />
-          </React.Fragment>
+          </main>
         </Router>
       </ApolloProvider>
     );
